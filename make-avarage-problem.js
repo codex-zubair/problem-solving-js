5e9d65228ebe9d4a9d5043a23455aa57f9b358c9
Rocko <codingwithrock@gmail.com>
2022-08-05 11:05:19 +0600

    make avarage problem solved

---
+++ make-avarage-problem.js
@@ -0,0 +1,14 @@
+function makeAverage(assignment1, assignment2, assignment3) {
+    let result = assignment1 + assignment2 + assignment3;
+
+    finallyResult = result / 3;
+
+    return finallyResult;
+
+}
+
+averageResult = makeAverage(60, 30, 60);
+
+console.log(averageResult);
+
+
